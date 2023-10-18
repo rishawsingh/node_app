992431b68e49c66753ac02a599fdfa290a0a8d28
Rishaw Kumar <rishawsingh333@gmail.com>
2023-10-18 17:14:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!! You have successfully deployed your Application using Jenkins!!
+          Welcome!! You have successfully deployed your Application using Jenkins!! Congrats
         </p>
         <a
           className="App-link"
